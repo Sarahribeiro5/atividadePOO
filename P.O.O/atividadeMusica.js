a25ad79c8998cb6fe31a59b9ee6c520adc70d71a
Sarah Ribeiro <sarahribeiro.tds2.senai@gmail.com>
2024-08-15 10:25:02 -0300

    musica

---
+++ P.O.O/atividadeMusica.js
@@ -0,0 +1,7 @@
+const musica = {
+    titulo: "Tomorrow",
+    artista: "BTS",
+    album: "Skool luv affair"
+} ;
+
+console.log(musica.nome);
---
+++ P.O.O/atividadefilme.js
@@ -0,0 +1,8 @@
+//filme
+const filme = {
+    titulo: "As branquelas",
+    diretor: "Keenen Ivory Wayans",
+    anoLancamento: 2004
+} ;
+
+console.log(filme.nome);
